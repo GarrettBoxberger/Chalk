d90951d00649017fcc055a864cc6307f386239c8
GarrettBoxberger <55366172+GarrettBoxberger@users.noreply.github.com>
2019-11-09 21:00:47 -0600

    inital commit

--- Chalk.js
+++ Chalk.js
@@ -1,3 +1,9 @@
-const chalk = require('chalk');
+const chalk = require("chalk");
  
-console.log(chalk.blue('Hello world!'));
+console.log(chalk.blue("Hello!"));
+console.log(chalk.green("My Name is Garrett."));
+console.log(chalk.green("I currently writing this short story at my friend's House."));
+console.log(chalk.red("All of my friends are having fun playing pool \nwhile Im over here being a good boy and doing home work."));
+console.log(chalk.cyan("But the upside is when I get finished with this I can join them."));
+console.log(chalk.yellow("I hope this is what you are looking for with this assignment.  :)"));
+
